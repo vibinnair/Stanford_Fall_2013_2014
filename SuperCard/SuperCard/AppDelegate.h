cd874dedd7cf42ba6e7ccee4a77afcd98bdb8001
Vibin Nair <vibin.nair@gmail.com>
2014-06-20 16:12:54 +0530

    Initial Commit.

---
+++ SuperCard/SuperCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Vibin Nair on 20/06/14.
+//  Copyright (c) 2014 Vibin Nair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SuperCard/SuperCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Vibin Nair on 20/06/14.
+//  Copyright (c) 2014 Vibin Nair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
